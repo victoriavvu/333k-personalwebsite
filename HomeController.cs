35fbddaf1eb0f242b32b0bdda9f63380e5e2d846
Victoria Vu <83671291+victoriavvu@users.noreply.github.com>
2022-10-09 21:31:04 -0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Vu_Victoria_HW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Interests()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+
+            string path = _environment.WebRootPath + "/files/Victoria Vu Resume 333K.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "Victoria Vu Resume 333K.pdf");
+        }
+     
+
+    }
+
+
+}
